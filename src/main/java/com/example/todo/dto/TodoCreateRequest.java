4d32ff4190b86a75ec23e31ad3304311f3433eff
cherry <cyg8937@gmail.com>
2026-06-17 01:39:50 +0900

    [cache test] not use --mount, added comment in java file

--- src/main/java/com/example/todo/dto/TodoCreateRequest.java
+++ src/main/java/com/example/todo/dto/TodoCreateRequest.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class TodoCreateRequest {
-
+// this is first test comment
     @NotBlank(message = "제목은 필수입니다.")
     @Size(max = 200, message = "제목은 200자 이하여야 합니다.")
     private String title;
